2f0c8e6bf1cc6bfbceb5dac48392019cfb1a9dde
Samuel Park <87939521+p-acid@users.noreply.github.com>
2025-10-16 20:52:36 +0900

    chore: radix-ui 설정 추가 (#4)

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,11 +1,17 @@
+import { Theme } from "@radix-ui/themes";
 import { RouterProvider } from "react-router-dom";
+
 import { QueryProvider } from "./query-provider";
 import { router } from "./router";
+
+import "@radix-ui/themes/styles.css";
 
 export function AppProvider() {
   return (
     <QueryProvider>
-      <RouterProvider router={router} />
+      <Theme>
+        <RouterProvider router={router} />
+      </Theme>
     </QueryProvider>
   );
 }
