a9e3109a1c760c153b350b3cefdff6250fa23f60
Samuel Park <87939521+p-acid@users.noreply.github.com>
2025-11-13 12:26:28 +0900

    fix: 일정 생성 완료 수신처 변경 (#17)

--- src/entities/chats/model/use-chat-store.ts
+++ src/entities/chats/model/use-chat-store.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
-import type { Chat, ErrorMessage } from "./chat.types";
+import type { ChatStream, MessageType } from "./chats.model";
+
+interface ClientChat extends Omit<ChatStream, "messageStreamType"> {
+  messageType: MessageType;
+}
 
 interface ChatState {
-  chats: Chat[];
-  error: ErrorMessage | null;
+  chats: ClientChat[];
   isWaitingResponse: boolean;
-  addChat: (chat: Chat) => void;
-  setError: (error: ErrorMessage | null) => void;
+  addChat: (chat: ClientChat) => void;
   setWaitingResponse: (isWaiting: boolean) => void;
   clearChats: () => void;
   reset: () => void;
@@ -14,7 +16,6 @@ interface ChatState {
 
 const initialState = {
   chats: [],
-  error: null,
   isWaitingResponse: false,
 };
 
@@ -25,12 +26,7 @@ export const useChatStore = create<ChatState>((set) => ({
     set((state) => ({
       chats: [...state.chats, chat],
     })),
-
-  setError: (error) => set({ error }),
-
   setWaitingResponse: (isWaiting) => set({ isWaitingResponse: isWaiting }),
-
   clearChats: () => set({ chats: [] }),
-
   reset: () => set(initialState),
 }));
