495e7c4e1171eb84e056d961d18de89288634735
p-acid <qkrtkstjd01@gmail.com>
2025-11-19 21:17:31 +0900

    style: 일정 생성 로딩 애니메이션 추가

--- src/entities/chats/ui/schedule-creating-chat/index.css.ts
+++ src/entities/chats/ui/schedule-creating-chat/index.css.ts
@@ -3,9 +3,14 @@ import { style } from "@vanilla-extract/css";
 export const content = style({
   display: "flex",
   flexDirection: "column",
-  gap: 12,
+  gap: 16,
   width: "100%",
   padding: "16px 14px",
   border: "1px solid var(--gray-a4)",
+  borderRadius: "var(--radius-6)",
+});
+
+export const loadingImage = style({
+  width: "100%",
   borderRadius: "var(--radius-6)",
 });
